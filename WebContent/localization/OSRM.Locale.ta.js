bb4a8f8ca011fab40615f5ef6c7c96a6ccef94be
naveenmtp <naveenmtp@gmail.com>
2012-11-23 06:56:23 +0530

    Added Tamil Translation

---
+++ WebContent/localization/OSRM.Locale.ta.js
@@ -0,0 +1,141 @@
+/*
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU AFFERO General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+or see http://www.gnu.org/licenses/agpl.txt.
+*/
+
+// OSRM localization
+// [Tamil language support]
+
+
+OSRM.Localization["ta"] = {
+// own language
+"CULTURE": "ta-IN",
+"LANGUAGE": "தமிழ்",
+// gui
+"GUI_START": "தொடங்கு",
+"GUI_END": "முடி",
+"GUI_RESET": "&nbsp;&nbsp;Reset&nbsp;&nbsp;",
+"GUI_ZOOM_ON_ROUTE": "வழி மீது பெரிதாக்கு",
+"GUI_ZOOM_ON_USER": "பயனர் மீது பெரிதாக்கு",
+"GUI_SEARCH": "&nbsp;&nbsp;Show&nbsp;&nbsp;",
+"GUI_REVERSE": "எதிர்புறமாக",
+"GUI_START_TOOLTIP": "துவக்கத்தை உள்ளிடவும்",
+"GUI_END_TOOLTIP": "முடிவை உள்ளிடவும்",
+"GUI_MAIN_WINDOW": "பிரதான சாளரம்",
+"GUI_ZOOM_IN": "பெரிதாக்கு",
+"GUI_ZOOM_OUT": "சிறிதாக்கு",
+// config
+"GUI_CONFIGURATION": "உள்ளமைவு",
+"GUI_LANGUAGE": "மொழி",
+"GUI_UNITS": "அலகுகள்",
+"GUI_KILOMETERS": "கிலோமீட்டர்கள்",
+"GUI_MILES": "மைல்கள்",
+// mapping
+"GUI_MAPPING_TOOLS": "வரைபடக் கருவிகள்",
+"GUI_HIGHLIGHT_UNNAMED_ROADS": "பெயரிடப்படாத தெருக்களை சுட்டிக்காட்டு",
+"GUI_SHOW_PREVIOUS_ROUTES": "முந்தைய வழித்தடங்களைக் காட்டு",
+"OPEN_JOSM": "JOSM",
+"OPEN_OSMBUGS": "OSM Bugs",
+// geocoder
+"SEARCH_RESULTS": "தேடல் முடிவுகள்",
+"FOUND_X_RESULTS": "%i முடிவுகள் உள்ளன",
+"TIMED_OUT": "நேரம் முடிந்துவிட்டது",
+"NO_RESULTS_FOUND": "ஒரு முடிவும் இல்லை",
+"NO_RESULTS_FOUND_SOURCE": "துவக்கத்திற்கு ஒரு முடிவும் இல்லை",
+"NO_RESULTS_FOUND_TARGET": "இலக்குக்கு ஒரு முடிவும் இல்லை",
+// routing
+"ROUTE_DESCRIPTION": "பாதை விளக்கம்",
+"GET_LINK_TO_ROUTE": "இணைப்பை உருவாக்கு",
+"GENERATE_LINK_TO_ROUTE": "இணைப்புக்காக காத்திருக்கிறது",
+"LINK_TO_ROUTE_TIMEOUT": "இல்லை",
+"GPX_FILE": "GPX கோப்பு",
+"DISTANCE": "தூரம்",
+"DURATION": "காலம்",
+"YOUR_ROUTE_IS_BEING_COMPUTED": "உங்கள் பாதை கணக்கிடப்படுகிறது",
+"NO_ROUTE_FOUND": "பாதை சாத்தியம் இல்லை",
+// printing
+"OVERVIEW_MAP": "வரைபட கண்ணோட்டம்",
+"NO_ROUTE_SELECTED": "எந்த பாதையும் தேர்ந்தெடுக்கப்படவில்லை",
+// routing engines
+"ENGINE_0": "கார் (விரைவாக)",
+// directions
+"N": "வடக்கு",
+"E": "கிழக்கு",
+"S": "தெற்கு",
+"W": "மேற்கு",
+"NE": "வடகிழக்கு",
+"SE": "தென்கிழக்கு",
+"SW": "தென்மேற்கு",
+"NW": "வடமேற்கு",
+// driving directions
+// %s: road name
+// %d: direction
+// [*]: will only be printed when there actually is a road name
+"DIRECTION_0":"[  <b>%s</b> மீது]  தெரியாத வழிமுறை",
+"DIRECTION_1":"[ <b>%s</b>மீது ] தொடர்க",
+"DIRECTION_2":"[  <b>%s</b> மீது] சற்று வலதுபுறம் திரும்புக",
+"DIRECTION_3":" <b>%s</b> மீது] வலதுபுறம் திரும்புக ",
+"DIRECTION_4":"[  <b>%s</b> மீது] தெளிவான வலதுபுறம் திரும்புக",
+"DIRECTION_5":"[ <b>%s</b> மீது] யூ வளைவு",
+"DIRECTION_6":"[ <b>%s</b> மீது] தெளிவான இடது பக்கம் திரும்புக",
+"DIRECTION_7":"[ <b>%s</b> மீது] இடது பக்கம் திரும்புக",
+"DIRECTION_8":"[ <b>%s</b> மீது] சற்றே இடது பக்கம் திரும்புக",
+"DIRECTION_10":" [  <b>%s</b> மீது] <b>%d</b> ல் செல்க",
+"DIRECTION_11-1":"[  <b>%s</b> மீது] ரவுன்டனாவைச்  சுற்றி முதல் வழியில் வெளியேறவும்",
+"DIRECTION_11-2":"[ <b>%s</b> மீது] ரவுன்டனாவைச்  சுற்றி இரண்டாம் வழியில் வெளியேறவும்",
+"DIRECTION_11-3":"[ <b>%s</b> மீது] ரவுன்டனாவைச்  சுற்றி மூன்றாம் வழியில் வெளியேறவும்",
+"DIRECTION_11-4":"[ <b>%s</b> மீது] ரவுன்டனாவைச்  சுற்றி நான்காம் வழியில் வெளியேறவும்",
+"DIRECTION_11-5":"[ <b>%s</b> மீது] ரவுன்டனாவைச்  சுற்றி ஐந்தாம் வழியில் வெளியேறவும்",
+"DIRECTION_11-6":"[ <b>%s</b> மீது] ரவுன்டனாவைச்  சுற்றி ஆறாம் வழியில் வெளியேறவும்",
+"DIRECTION_11-7":"[ <b>%s</b> மீது] ரவுன்டனாவைச்  சுற்றி ஏழாம் வழியில் வெளியேறவும்",
+"DIRECTION_11-8":"[ <b>%s</b> மீது] ரவுன்டனாவைச்  சுற்றி எட்டாம் வழியில் வெளியேறவும்",
+"DIRECTION_11-9":"[ <b>%s</b> மீது] ரவுன்டனாவைச்  சுற்றி ஒன்பதாம் வழியில் வெளியேறவும்",
+"DIRECTION_11-x":"ச்ச்[ onto <b>%s</b>]",
+"DIRECTION_15":"நீங்கள் உங்கள் இலக்கை அடைந்துவிட்டீர்கள்",
+// notifications
+"NOTIFICATION_MAINTENANCE_HEADER":	"Scheduled Maintenance",
+"NOTIFICATION_MAINTENANCE_BODY":	"The OSRM Website is down for a scheduled maintenance. " + 
+									"Please be patient while required updates are performed. " +
+									"The site will be back online shortly." +
+									"<br/><br/>" + 
+									"In the meantime you may want to go out an map a friendly neighborhood near you..." + 
+									"<br/><br/><br/>[OSRM]",
+"NOTIFICATION_LOCALIZATION_HEADER":	"Did you know? You can change the display language.",
+"NOTIFICATION_LOCALIZATION_BODY":	"You can use the pulldown menu in the upper left corner to select your favorite language. " +
+									"<br/><br/>" +
+									"Don't despair if you cannot find your language of choice. " +
+									"If you want, you can help to provide additional translations! " +
+									"Visit <a href='https://github.com/DennisSchiefer/Project-OSRM-Web'>here</a> for more information.",
+"NOTIFICATION_CLICKING_HEADER":		"Did you know? You can click on the map to set route markers.",
+"NOTIFICATION_CLICKING_BODY":		"You can click on the map with the left mouse button to set a source marker (green) or a target marker (red), " +
+									"if the source marker already exists. " +
+									"The address of the selected location will be displayed in the boxes to the left. " + 
+									"<br/><br/>" +
+									"You can delete a marker by clicking on it again with the left mouse button.",
+"NOTIFICATION_DRAGGING_HEADER":		"Did you know? You can drag each route marker on the map.",
+"NOTIFICATION_DRAGGING_BODY":		"You can drag a marker by clicking on it with the left mouse button and holding the button pressed. " +
+									"Then you can move the marker around the map and the route will be updated instantaneously. " +
+									"<br/><br/>" +
+									"You can even create intermediate markers by dragging them off of the main route! ",
+// do not translate below this line
+"GUI_LEGAL_NOTICE": "Routing by <a href='http://project-osrm.org/'>Project OSRM</a> - Geocoder by <a href='http://wiki.openstreetmap.org/wiki/Nominatim'>Nominatim</a> - OSRM hosting by <a href='http://algo2.iti.kit.edu/'>KIT</a>",
+"GUI_DATA_TIMESTAMP": "data: ",
+"GUI_VERSION": "gui: ",
+"QR": "QR"
+};
+
+// set GUI language on load
+if( OSRM.DEFAULTS.LANUGAGE_ONDEMAND_RELOADING == true )
+	OSRM.Localization.setLanguage("en");
